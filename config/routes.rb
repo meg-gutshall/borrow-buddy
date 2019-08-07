30b4c6f902510566b38c07c5f6a8072402496be1
Meg Gutshall <meghan.gutshall@gmail.com>
2019-08-07 10:34:08 -0400

    Set up and nest routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,23 @@ Rails.application.routes.draw do
   # Home
   root to: 'application#home'
 
-  devise_for :lenders
-  resources :borrows
-  resources :recipients
-  resources :items
+  scope :lender do
+    devise_for :lenders, path: ""
+    resources :borrows
+    resources :recipients, except: [:show]
+    resources :items, except: [:show]
+  end
+
+  get "/lender/:id", to: "lenders#show", as: "lender"
+  
+  resources :items, only: [:show] do
+    resources :recipients
+    resources :borrows
+  end
+
+  resources :recipients, only: [:show] do
+    resources :items
+    resources :borrows
+  end
 
 end
