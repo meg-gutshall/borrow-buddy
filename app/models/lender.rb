8fe7a89a6315e4a526cb2ea60de0e904e8becd36
Meg Gutshall <meghan.gutshall@gmail.com>
2019-08-07 10:34:29 -0400

    Adjust lender model associations

--- app/models/lender.rb
+++ app/models/lender.rb
@@ -1,6 +1,6 @@
 class Lender < ApplicationRecord
   has_many :items
   has_many :borrows, through: :items
-  has_many :recipients, through: :items
+  
   devise :database_authenticatable, :omniauthable, :registerable, :recoverable, :rememberable, :validatable
 end
