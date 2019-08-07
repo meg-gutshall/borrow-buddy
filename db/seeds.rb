3b681bb1e4f55c412e7eb9b0fbdd0694d13c59a0
Meg Gutshall <meghan.gutshall@gmail.com>
2019-08-07 10:54:47 -0400

    Create seeds for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,61 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+# Lender objects
+meg = Lender.create(
+  email: "meg@meg.com",
+  password: "password"
+)
+
+pip = Lender.create(
+  email: "pip@pip.com",
+  password: "password"
+)
+
+# Item objects
+3.times do
+  Item.create(
+    lender_id: 1,
+    name: Faker::Book.title,
+    category: "book"
+  )
+end
+
+2.times do
+  Item.create(
+    lender_id: 1,
+    name: Faker::Music.album,
+    category: "album"
+  )
+end
+
+4.times do
+  Item.create(
+    lender_id: 2,
+    name: Faker::Game.title,
+    category: "game"
+  )
+end
+
+# Recipient objects
+7.times do
+  Recipient.create(
+    name: Faker::Name.first_name,
+    email: Faker::Internet.free_email,
+    phone: Faker::PhoneNumber.cell_phone
+  )
+end
+
+# Borrow objects
+11.times do
+  Borrow.create(
+    recipient_id: Faker::Number.between(1, 7),
+    item_id: Faker::Number.between(1, 9),
+    days_borrowed: Faker::Number.between(1, 17),
+    reminders_sent: Faker::Number.between(1, 3),
+    returned: Faker::Boolean.boolean
+  )
+end
