d4f4c832bbeed642d6951164ec543a3816cd1793
Meg Gutshall <meghan.gutshall@gmail.com>
2019-08-07 12:27:01 -0400

    Add validations

--- app/models/borrow.rb
+++ app/models/borrow.rb
@@ -1,4 +1,9 @@
 class Borrow < ApplicationRecord
   belongs_to :item
   belongs_to :recipient
+
+  validates :days_borrowed, :reminders_sent, presence: true
+  validates :returned, inclusion: { in: [true, false] }
+  validates :days_borrowed, numericality: { only_integer: true, greater_than: 0 }
+  validates :reminders_sent, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,8 @@ class Item < ApplicationRecord
   belongs_to :lender
   has_many :borrows
   has_many :recipients, through: :borrows
+
+  validates_associated :lender
+  validates_associated :borrows
+  validates :name, :category, presence: true
 end
