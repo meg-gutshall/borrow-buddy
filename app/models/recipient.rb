4b9dfb3bd6bc6e214bb03834b786302d5fea69e9
Meg Gutshall <meghan.gutshall@gmail.com>
2019-08-10 18:27:57 -0400

    Remove recipient phone format validation

--- app/models/recipient.rb
+++ app/models/recipient.rb
@@ -5,5 +5,5 @@ class Recipient < ApplicationRecord
   validates_associated :borrows
   validates :name, presence: true
   validates :email, format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i }, allow_blank: true
-  validates :phone, format: { with: /\A[(]\d{3}[)] \d{3}-\d{4}\z/ }, allow_blank: true
+  # validates :phone, format: { with: /\A[(]\d{3}[)] \d{3}-\d{4}\z/ }, allow_blank: true
 end
