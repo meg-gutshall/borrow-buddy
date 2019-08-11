abfb03114f527d7084e63c30d7d3614511b6ddbb
Meg Gutshall <meghan.gutshall@gmail.com>
2019-08-10 20:04:24 -0400

    Add helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,15 @@ class ApplicationController < ActionController::Base
   def home
     render "home/home"
   end
-  
+
+  private
+    # Returns the current lender or 'nil' if none exists
+    # def current_lender
+    #   Lender.find_by(id: session[:lender_id])
+    # end
+
+    # Returns true or 'nil' if there is no current lender
+    # def logged_in?
+    #   !!current_lender
+    # end
 end
