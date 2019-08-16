159a1a1b774194423a99ef3a5788c244dae7a144
Meg Gutshall <meghan.gutshall@gmail.com>
2019-08-16 15:51:09 -0400

    Add custom routes per scope methods

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   devise_for :lenders, controllers: { omniauth_callbacks: 'lenders/omniauth_callbacks' }
 
   resources :lenders, only: [:show] do
+    get 'borrows/current_borrows', to: 'borrows#current_borrows', as: 'current_borrows'
+    get 'borrows/returned_borrows', to: 'borrows#returned_borrows', as: 'returned_borrows'
     resources :borrows
     resources :recipients
     resources :items
