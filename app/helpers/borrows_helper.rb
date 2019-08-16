f1ec520aaf4863a4d8e28681056bddfe9a0efeab
Meg Gutshall <meghan.gutshall@gmail.com>
2019-08-15 21:45:07 -0400

    Create conditional borrow new form view helper

--- app/helpers/borrows_helper.rb
+++ app/helpers/borrows_helper.rb
@@ -1,5 +1,17 @@
 module BorrowsHelper
+  
   def boolean_display(attribute)
     attribute == true ? "Yes" : "No"
   end
+
+  def borrow_origin
+    if params[:item_id]
+      render 'borrows/form_from_item'
+    elsif params[:recipient_id]
+      render 'borrows/form_from_recipient'
+    else
+      render 'borrows/form_from_borrow'
+    end
+  end
+
 end
