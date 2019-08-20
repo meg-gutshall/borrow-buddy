5b28c842aca070477a4e7ce4e3ef9b4f50bda0db
Meg Gutshall <meghan.gutshall@gmail.com>
2019-08-20 13:53:07 -0400

    Add locals to #borrow_origin form rendering helper

--- app/helpers/borrows_helper.rb
+++ app/helpers/borrows_helper.rb
@@ -2,11 +2,11 @@ module BorrowsHelper
 
   def borrow_origin
     if params[:item_id]
-      render 'borrows/form_from_item'
+      render 'borrows/form_from_item', locals: { form_object: @item }
     elsif params[:recipient_id]
-      render 'borrows/form_from_recipient'
+      render 'borrows/form_from_recipient', locals: { form_object: @recipient }
     else
-      render 'borrows/form_from_borrow'
+      render 'borrows/form_from_borrow', locals: { form_object: current_lender }
     end
   end
 
