cc4a286293b3419ad965bf620fdc9a8893e90173
Meg Gutshall <meghan.gutshall@gmail.com>
2019-08-20 20:02:34 -0400

    Add partials for new borrow forms

--- app/helpers/borrows_helper.rb
+++ app/helpers/borrows_helper.rb
@@ -2,11 +2,11 @@ module BorrowsHelper
 
   def borrow_origin
     if params[:item_id]
-      render 'borrows/form_from_item', locals: { form_object: @item }
+      render partial: "borrows/form_partials/form_from_item", locals: { form_top_level: @item }
     elsif params[:recipient_id]
-      render 'borrows/form_from_recipient', locals: { form_object: @recipient }
+      render partial: "borrows/form_partials/form_from_recipient", locals: { form_top_level: @recipient }
     else
-      render 'borrows/form_from_borrow', locals: { form_object: current_lender }
+      render partial: "borrows/form_partials/form_from_borrow", locals: { form_top_level: current_lender }
     end
   end
 
