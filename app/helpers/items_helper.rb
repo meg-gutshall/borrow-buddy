a417725c75f726cf9ea65e631476f48cbb1aab06
Meg Gutshall <meghan.gutshall@gmail.com>
2019-08-21 17:18:55 -0400

    Add partials for no information edge cases

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,9 @@
 module ItemsHelper
+  def no_items_tracked
+    if current_lender.items.empty?
+      render partial: "items/index_partials/no_items"
+    else
+      render partial: "items/index_partials/items_table_body"
+    end
+  end
 end
--- app/helpers/recipients_helper.rb
+++ app/helpers/recipients_helper.rb
@@ -1,2 +1,9 @@
 module RecipientsHelper
+  def no_recipients_tracked
+    if current_lender.recipients.empty?
+      render partial: "recipients/index_partials/no_recipients"
+    else
+      render partial: "recipients/index_partials/recipients_table_body"
+    end
+  end
 end
