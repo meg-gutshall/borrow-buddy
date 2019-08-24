c2f0dcd0d14895b073f39356aa0cdd500fcd0566
Meg Gutshall <meghan.gutshall@gmail.com>
2019-08-23 21:33:49 -0400

    Add email validation error message

--- app/models/borrower.rb
+++ app/models/borrower.rb
@@ -3,7 +3,7 @@ class Borrower < ApplicationRecord
   has_many :items, through: :loans
 
   validates :name, presence: true
-  validates :email, format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i }, allow_blank: true
+  validates :email, format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i, message: "Please use proper formatting when entering the borrower's email." }, allow_blank: true
   # validates :phone, format: { with: /\A[(]\d{3}[)] \d{3}-\d{4}\z/ }, allow_blank: true
 
   # See `application_record.rb` for scope methods
