5bf1a51a7f0b13a573836f7968b7bd58aa9778ce
Meg Gutshall <meghan.gutshall@gmail.com>
2020-10-26 14:56:18 -0400

    Remove scopable index from loans

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_06_163015) do
+ActiveRecord::Schema.define(version: 2019_08_11_192746) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,12 +53,9 @@ ActiveRecord::Schema.define(version: 2019_09_06_163015) do
     t.boolean "returned", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "scopable_type"
-    t.bigint "scopable_id"
     t.index ["borrower_id"], name: "index_loans_on_borrower_id"
     t.index ["item_id"], name: "index_loans_on_item_id"
     t.index ["lender_id"], name: "index_loans_on_lender_id"
-    t.index ["scopable_type", "scopable_id"], name: "index_loans_on_scopable_type_and_scopable_id"
   end
 
 end
