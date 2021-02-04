99676952ed621b9ff6a51b58f54191b44fb5a1f3
Meg Gutshall <meghan.gutshall@gmail.com>
2021-02-04 18:15:40 -0500

    Fix faker syntax

--- spec/factories/loans.rb
+++ spec/factories/loans.rb
@@ -3,8 +3,8 @@ FactoryBot.define do
     lender
     item
     borrower
-    days_borrowed { Faker::Number.between(1, 17) }
-    reminders_sent { Faker::Number.between(1, 3) }
+    days_borrowed { Faker::Number.between(from: 1, to: 17) }
+    reminders_sent { Faker::Number.between(from: 1, to: 3) }
     returned { Faker::Boolean.boolean }
   end
 end
