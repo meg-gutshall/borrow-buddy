aa2f7e3e18f710912a7ed65ea4c513f0b6b92358
Meg Gutshall <meghan.gutshall@gmail.com>
2021-02-04 18:18:55 -0500

    Add trait and create nested factory for lender

--- spec/factories/lenders.rb
+++ spec/factories/lenders.rb
@@ -2,11 +2,13 @@ FactoryBot.define do
   factory :lender do
     email { Faker::Internet.free_email }
     password { Faker::Internet.password }
-  end
-end
 
-def lender_with_loan
-  FactoryBot.create(:lender) do |lender|
-    FactoryBot.create(:loan, lender: lender)
+    trait :with_loans do
+      after(:build) do |lender|
+        lender.loans << build(:loan, lender: lender) 
+      end
+    end
+
+    factory :lender_with_loans, traits: :with_loans
   end
 end
