a30825d2de360629081bf9b654fc4922d1aa1307
Meg Gutshall <meghan.gutshall@gmail.com>
2021-02-04 20:07:40 -0500

    Update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,10 @@
 # This file should contain all the record creation needed to seed the database with its default values. The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-
 require 'faker'
+
+Lender.destroy_all
+Loan.destroy_all
+Item.destroy_all
+Borrower.destroy_all
 
 # Lender objects
 meg = Lender.create!(
@@ -12,34 +16,42 @@ pip = Lender.create!(
   email: "pip@pip.com",
   password: "password"
 )
-puts "Created #{Lender.count} lenders."
+
+puts "Created #{Lender.all.count} lenders."
 
 # Item objects
-3.times do
+Faker::Number.between(from: 5, to: 9).times do
   Item.create!(
     name: Faker::Book.title,
     category: "book"
   )
 end
 
-2.times do
+Faker::Number.between(from: 9, to: 13).times do
   Item.create!(
     name: Faker::Music.album,
     category: "album"
   )
 end
 
-4.times do
+Faker::Number.between(from: 3, to: 5).times do
   Item.create!(
     name: Faker::Game.title,
     category: "game"
   )
 end
 
-puts "Created #{Item.count} items."
+Faker::Number.between(from: 4, to: 8).times do
+  Item.create!(
+    name: Faker::Movie.title,
+    category: "movie"
+  )
+end
+
+puts "Created #{Item.all.count} items."
 
 # Borrower objects
-7.times do
+17.times do
   Borrower.create!(
     name: Faker::Name.first_name,
     email: Faker::Internet.free_email,
@@ -47,106 +59,29 @@ puts "Created #{Item.count} items."
   )
 end
 
-puts "Created #{Borrower.count} borrowers."
+puts "Created #{Borrower.all.count} borrowers."
 
 # Loan objects
-Loan.create!(
-  lender_id: 1,
-  borrower_id: 1,
-  item_id: 1,
-  days_borrowed: Faker::Number.between(1, 17),
-  reminders_sent: Faker::Number.between(1, 3),
-  returned: Faker::Boolean.boolean
-)
+17.times do
+  Loan.create!(
+    lender: meg,
+    borrower: Borrower.all.sample,
+    item: Item.all.sample,
+    days_borrowed: Faker::Number.between(from: 1, to: 17),
+    reminders_sent: Faker::Number.between(from: 1, to: 3),
+    returned: Faker::Boolean.boolean(true_ratio: 0.3)
+  )
+end
 
-Loan.create!(
-  lender_id: 1,
-  borrower_id: 2,
-  item_id: 2,
-  days_borrowed: Faker::Number.between(1, 17),
-  reminders_sent: Faker::Number.between(1, 3),
-  returned: Faker::Boolean.boolean
-)
+14.times do
+  Loan.create!(
+    lender: pip,
+    borrower: Borrower.all.sample,
+    item: Item.all.sample,
+    days_borrowed: Faker::Number.between(from: 1, to: 17),
+    reminders_sent: Faker::Number.between(from: 1, to: 3),
+    returned: Faker::Boolean.boolean(true_ratio: 0.4)
+  )
+end
 
-Loan.create!(
-  lender_id: 1,
-  borrower_id: 3,
-  item_id: 3,
-  days_borrowed: Faker::Number.between(1, 17),
-  reminders_sent: Faker::Number.between(1, 3),
-  returned: Faker::Boolean.boolean
-)
-
-Loan.create!(
-  lender_id: 1,
-  borrower_id: 4,
-  item_id: 4,
-  days_borrowed: Faker::Number.between(1, 17),
-  reminders_sent: Faker::Number.between(1, 3),
-  returned: Faker::Boolean.boolean
-)
-
-Loan.create!(
-  lender_id: 1,
-  borrower_id: 5,
-  item_id: 5,
-  days_borrowed: Faker::Number.between(1, 17),
-  reminders_sent: Faker::Number.between(1, 3),
-  returned: Faker::Boolean.boolean
-)
-
-Loan.create!(
-  lender_id: 2,
-  borrower_id: 6,
-  item_id: 6,
-  days_borrowed: Faker::Number.between(1, 17),
-  reminders_sent: Faker::Number.between(1, 3),
-  returned: Faker::Boolean.boolean
-)
-
-Loan.create!(
-  lender_id: 2,
-  borrower_id: 7,
-  item_id: 7,
-  days_borrowed: Faker::Number.between(1, 17),
-  reminders_sent: Faker::Number.between(1, 3),
-  returned: Faker::Boolean.boolean
-)
-
-Loan.create!(
-  lender_id: 2,
-  borrower_id: 6,
-  item_id: 8,
-  days_borrowed: Faker::Number.between(1, 17),
-  reminders_sent: Faker::Number.between(1, 3),
-  returned: Faker::Boolean.boolean
-)
-
-Loan.create!(
-  lender_id: 2,
-  borrower_id: 1,
-  item_id: 9,
-  days_borrowed: Faker::Number.between(1, 17),
-  reminders_sent: Faker::Number.between(1, 3),
-  returned: Faker::Boolean.boolean
-)
-
-Loan.create!(
-  lender_id: 1,
-  borrower_id: 2,
-  item_id: 3,
-  days_borrowed: Faker::Number.between(1, 17),
-  reminders_sent: Faker::Number.between(1, 3),
-  returned: Faker::Boolean.boolean
-)
-
-Loan.create!(
-  lender_id: 2,
-  borrower_id: 5,
-  item_id: 6,
-  days_borrowed: Faker::Number.between(1, 17),
-  reminders_sent: Faker::Number.between(1, 3),
-  returned: Faker::Boolean.boolean
-)
-
-puts "Created #{Loan.count} loans."
+puts "Created #{Loan.all.count} loans."
